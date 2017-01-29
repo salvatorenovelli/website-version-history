f6c96c21e378f478e09ea619eff74c6fc4679b26
snovelli <salvatore.novelli@gmail.com>
2017-01-29 17:13:20 +0000

    INITIAL COMMIT

---
+++ src/main/java/com/github/salvatorenovelli/seo/WebsiteVersionHistoryApplication.java
@@ -0,0 +1,12 @@
+package com.github.salvatorenovelli.seo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteVersionHistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteVersionHistoryApplication.class, args);
+	}
+}
