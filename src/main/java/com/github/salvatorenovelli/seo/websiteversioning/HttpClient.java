e072ba083440361aae23b4a7e36f70c61aacfacf
Adriana <adriana021279@gmail.com>
2017-04-01 19:20:27 +0100

    Removed unused classes

--- src/main/java/com/github/salvatorenovelli/seo/websiteversioning/HttpClient.java
+++ src/main/java/com/github/salvatorenovelli/seo/websiteversioning/HttpClient.java
@@ -9,23 +9,8 @@ import java.io.IOException;
 import java.net.URI;
 
 public class HttpClient {
-
-
     public Document get(URI path) throws IOException {
-
-//        CloseableHttpClient httpclient = HttpClients.createDefault();
-//        HttpGet httpGet = new HttpGet("http://targethost/homepage");
-//        CloseableHttpResponse response1 = httpclient.execute(httpGet);
-
-
-        try {
             Connection connect = Jsoup.connect(path.toASCIIString());
             return connect.get();
-
-        } finally {
-
-        }
-
-
     }
 }
--- src/main/java/com/github/salvatorenovelli/seo/websiteversioning/model/HtmlPage.java
+++
@@ -1,4 +0,0 @@
-package com.github.salvatorenovelli.seo.websiteversioning.model;
-
-public class HtmlPage {
-}
