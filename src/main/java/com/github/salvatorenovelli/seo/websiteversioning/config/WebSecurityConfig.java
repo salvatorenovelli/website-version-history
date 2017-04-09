2e1c3303aeb0f6f7c4aef5160c22356b8a7681af
snovelli <salvatore.novelli@gmail.com>
2017-04-09 08:27:48 +0100

    Introducing auth

---
+++ src/main/java/com/github/salvatorenovelli/seo/websiteversioning/config/WebSecurityConfig.java
@@ -0,0 +1,39 @@
+package com.github.salvatorenovelli.seo.websiteversioning.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth,
+                                @Value("${default.user}") String defaultUser,
+                                @Value("${default.password}") String defaultPassword) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser(defaultUser).password(defaultPassword).roles("USER");
+    }
+}
